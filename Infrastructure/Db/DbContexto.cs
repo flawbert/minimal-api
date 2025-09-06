3698d4914a7c2b2ecda267c835a6078cffbfebe0
flawbert <contato.flawbertlorran@hotmail.com>
2025-09-06 18:10:58 -0300

    Adding database logic

---
+++ Infrastructure/Db/DbContexto.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalApi.Domain.Entities;
+
+namespace MinimalApi.Infrastructure.Db;
+
+public class DbContexto : DbContext
+{
+
+    private readonly IConfiguration _configurationAppSettings;
+
+    public DbContexto(IConfiguration configurationAppSettings)
+    {
+        _configurationAppSettings = configurationAppSettings;
+    }
+
+    public DbSet<Admin> Admins { get; set; } = default!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            var connectionString = _configurationAppSettings.GetConnectionString("mysql")?.ToString();
+            if(!string.IsNullOrEmpty(connectionString))
+            {
+                optionsBuilder.UseMySql(
+                    connectionString,
+                    ServerVersion.AutoDetect(connectionString));
+            }
+            
+        }
+        
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalApi.Infrastructure.Db;
+using MinimalApi.DTOS;
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DbContexto>(options =>
+{
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("mysql"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql"))
+    );
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "E aí Pessoal!");
 
-app.MapPost("/login", (MinimalApi.DTOS.LoginDTO loginDTO) =>
+app.MapPost("/login", (LoginDTO loginDTO) =>
 {
     if (loginDTO.Email == "admin@teste.com" && loginDTO.Password == "123456")
         return Results.Ok("Login successful");
